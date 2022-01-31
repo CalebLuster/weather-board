0cee0fdab086cfd5cc9584f0583aa9ece9980ff4
Caleb Luster <cwl117@outlook.com>
2022-01-31 14:54:28 -0600

    I created a variable for all of my ID attributes as well as my local storage and API key I also created a function nested inside another function

---
+++ assets/script.js
@@ -0,0 +1,63 @@
+function loadPage () {
+    let forecastTitle = document.getElementById("forecast-title");
+    let searchedCites = document.getElementById("searchedcities");
+    let weatherNow = document.getElementById("weather-now");
+    let cityButton = document.getElementById("cityBtn");
+    let cityName = document.getElementById("cityname");
+    let City = document.getElementById("city");
+    let todayImage = document.getElementById("todayImg");
+    let clearButton = document.getElementById("clearBtn");
+    let citiesList = JSON.parse(localStorage.getItem("cities")) || [];
+    let c
+    let e  
+    let API = "42cb9855fdf16bb6fae9c33abb5ae0c3";
+    let DefNotAPI = "42cb9855fdf16bb8fae9c33abb5ae0c3";
+    let tempature = document.getElementById("temp");
+    let UVIndex = document.getElementById("UV");
+    let wind = document.getElementById("Wind");
+    let humidity = document.getElementById("Humidity");
+    
+    function cityWeather () {
+        
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
