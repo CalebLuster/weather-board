dee0754f5663da58c9267433b2a908ddb56cc2a0
Caleb Luster <cwl117@outlook.com>
2022-02-06 21:15:47 -0600

    could not get the app fully working in time

--- assets/script.js
+++ assets/script.js
@@ -8,23 +8,25 @@ function loadPage () {
     let todayImage = document.getElementById("todayImg");
     let clearButton = document.getElementById("clearBtn");
     let citiesList = JSON.parse(localStorage.getItem("cities")) || [];
-    let API = "42cb9855fdf16bb6fae9c33abb5ae0c3";
-    let DefNotAPI = "42cb9855fdf16bb8fae9c33abb5ae0c3";
+    const DefNotAPI = "1eafbc79bda58d65db48fb142ec8ed92"; 
     let tempature = document.getElementById("temp");
     let UVIndex = document.getElementById("UV");
     let wind = document.getElementById("Wind");
     let humidity = document.getElementById("Humidity");
     
-    function cityWeather(cityHeader) {
-        let URL = "https://openweathermap.org/data/2.5/weather?q=" + cityHeader + "&appid=" + DefNotAPI;
+    function cityWeather(City) {
+        let URL = "https://api.openweathermap.org/data/2.5/weather?q=" + City + "&appid=" + DefNotAPI;
+        
         axios.get(URL)
             .then(function (response) {
-                weatherNow.classList.remove ("d-none");
+                // console.log(response.data)
+                weatherNow.classList.remove("d-none");
                 let date = new Date(response.data.dt * 1000);
                 let day = date.getDate();
                 let months = date.getMonth() + 1;
                 let year = date.getFullYear();
                 let nwImg = response.data.weather[0].icon;
+                
 
                 cityName.innerHTML = response.data.name + " (" + months + "/" + day + "/" + year + ") ";
 
@@ -34,13 +36,16 @@ function loadPage () {
                 humidity.innerHTML = "Humidity: " + response.data.main.humidity + "%";
                 todayImage.setAttribute("src", "https://openweathermap.org/img/wn/" + nwImg + "@2x.png");
                 todayImage.setAttribute("alt", response.data.weather[0].description);
-                let longitude = response.data.coord.longitude;
-                let latitude = response.data.coord.lat;
+                let longitude 
+                let latitude 
                 let UVURL = "https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + latitude + "&lon=" + longitude + "&appid=" + DefNotAPI + "&cnt=1";
                 axios.get(UVURL)
                     .then(function (response) {
                         let UVindex = document.createElement("span");
                         
+                         longitude = response.data.coord.longitude;
+                         latitude = response.data.coord.lat;
+
                         if (response.data[0].value < 4 ) {
                             UVindex.setAttribute("class", "badge-sucess badge");
                         }
@@ -55,18 +60,22 @@ function loadPage () {
                         UVIndex.append(UVindex);
                     });
                     let cityTitle = response.data.id;
-                    let URLfuture = "https://api.openweathermap.org/data/2.5/uvi/forecast?id=" + cityTitle + "&appid=" + DefNotAPI;
-                    axios.get(URLfuture)
+                     longitude = response.data.coord.lon;
+                    latitude = response.data.coord.lat;
+                    let forecastURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&appid=" + DefNotAPI + "&cnt=1";
+                    axios.get(forecastURL)
                         .then(function (response) {
                         forecastTitle.classList.remove("d-none");
                         let futureWeather = document.querySelectorAll(".forecast");
                         for (i = 0; i < futureWeather.length; i++) {
                             futureWeather[i].innerHTML = "";
-                            let futureYear = forecastDate.getFullYear();
-                            let futureMonth = forecastDate.getMonth(); + 1;
-                            let futureDay = forecastDate.getDate();
                             let futureUV = i * 8 + 4;
+                            
                             let futureConstant = new Date(response.data.list[futureUV].dt * 1000);
+                            let futureYear = forecastConstant.getFullYear();
+                            let futureMonth = forecastConstant.getMonth(); + 1;
+                            let futureDay = forecastConstant.getDate();
+                            
                             let futureConsistent = document.createElement("p");
                                 futureConsistent.setAttribute("class", "forecast-date mt-3 mb-0");
                                 futureConsistent.innerHTML = futureMonth + "/" + futureDay + "/" + futureYear;
@@ -84,6 +93,7 @@ function loadPage () {
                         }
 
                      })
+                
             });
 
     }
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Welcome To the Weather Dashboard!
+
+This app contains features that can find the desired cities weather and weekly forecast!
+
+Just simply type in your city press enter and BAM you will have all your weather and forecast needs met!
+
+Here is a link to the deployed page! https://calebluster.github.io/weather-board/
+
+Here is a screen shot of the project: ![Screenshot 29](<assets/images/Screenshot%20(29).png>)
